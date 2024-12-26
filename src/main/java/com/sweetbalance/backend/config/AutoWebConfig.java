5bca462e2526196a130812873e35934b3e6260c1
kisusu <kikisu115@naver.com>
2024-12-26 16:58:18 +0900

    feat: CORS 설정 추가

--- src/main/java/com/sweetbalance/backend/config/AutoWebConfig.java
+++ src/main/java/com/sweetbalance/backend/config/AutoWebConfig.java
@@ -2,17 +2,19 @@ package com.sweetbalance.backend.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
-public class AutoWebConfig { // implements WebMvcConfigurer {
+public class AutoWebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
-//                .exposedHeaders("Set-Cookie", "Authorization")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
+                .exposedHeaders("Set-Cookie", "Authorization")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT");
+    }
 }
 
